ec1672564516a554cc9b845d8865f0b8a13d51a8
maskom_team <maskom_team@ma-malnukananga.sch.id>
2025-11-20 15:23:36 +0000

    Fix Schema import typo in migration files
    The PR correctly added missing 'use Hyperf\DbConnection\Db;' imports but missed fixing the Schema import typo.
    Changed 'use Hypervel\Support\Facades\Schema;' to 'use Hyperf\Support\Facades\Schema;' in all migration files to maintain consistency with the Hyperf framework being used.
    
    Fixes additional issue in database migrations

--- database/migrations/2025_05_18_002835_create_ppdb_table.php
+++ database/migrations/2025_05_18_002835_create_ppdb_table.php
@@ -4,7 +4,7 @@ declare (strict_types = 1);
 
 use Hyperf\Database\Migrations\Migration;
 use Hyperf\Database\Schema\Blueprint;
-use Hypervel\Support\Facades\Schema;
+use Hyperf\Support\Facades\Schema;
 use Hyperf\DbConnection\Db;
 
 return new class extends Migration
